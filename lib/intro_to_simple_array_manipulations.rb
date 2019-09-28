76f6f419d610b4d8ef756418353c18c54c80b2df
Anika Christopher <anika.mchristopher@gmail.com>
2019-09-28 00:19:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -32,5 +32,12 @@ def using_shift
    @brands_removed = shift_with_args(@ice_cream_brands)
   end
   
+  def using_concat
+    @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+    more_favs = ["sports cars", "flatiron school"]
+    @all_my_favs = using_concat(@my_favorite_things, more_favs)
+  end
+  
+  
 
   
