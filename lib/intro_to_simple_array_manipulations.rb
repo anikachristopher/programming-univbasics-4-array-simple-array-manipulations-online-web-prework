0d536eb79decf5e739cfad6e3ed3358ca53072c2
Anika Christopher <anika.mchristopher@gmail.com>
2019-09-28 00:20:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,6 +38,13 @@ def using_shift
     @all_my_favs = using_concat(@my_favorite_things, more_favs)
   end
   
+  def using_insert
+      @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+    @another_language = "Python"
+    @new_array = using_insert(@list_of_programming_languages, @another_language)
+  end
   
+  def using_uniq
+    
 
   
